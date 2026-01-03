5c54e093e15bee9934296d111b2f290019e24a34
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-01-03 21:51:52 +0600

    dot and bracket notation added

---
+++ dotAndBracketNotation.js
@@ -0,0 +1,13 @@
+const personalData = {
+    name: 'Sharafat rohoman',
+    age: 20,
+    friends: ['faru', 'moni'],
+    salary: 25000,
+    10: 'secret code'
+}
+
+console.log(personalData.name)
+console.log(personalData['name'])
+
+// console.log(personalData.10) // not defind
+console.log(personalData['10'])
