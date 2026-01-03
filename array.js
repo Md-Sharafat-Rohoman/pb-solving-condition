5b8ef5f88dbc062e628d6d7cbed1b58a83cbfce6
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-01-03 19:08:25 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+const friend = ['sharafat', 'faru', 'rohoman', 'muntaha'];
+console.log(friend.length)
+console.log(friend.includes('muntaha'))
+console.log(friend.indexOf('muntaha'))
+friend.push('moni')
+console.log(friend)
+friend.pop();
+console.log(friend);
+friend.shift();
+console.log(friend);
+friend.unshift('rohoman');
+console.log(friend)
+
+
---
+++ condition.js
@@ -0,0 +1,7 @@
+const number = 10;
+if(number<=20 && number<=40){
+    console.log('faru moni is a good student')
+}
+else{
+    console.log(false)
+}
